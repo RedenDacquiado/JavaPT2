5542d778c58244bdc11ffb029cedb36cf3960cdf
RedenDacquiado <Rijdacquiado@mcm.edu.ph>
2025-01-28 12:38:32 +0800

    Arithmetic Calculator

---
+++ SimpleCalculator.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class SimpleCalculator{
+    public static void main(String[] args) {
+        Scanner imput = new Scanner(System.in);
+        
+        System.out.print("Enter First Number:");
+        int number = imput.nextInt();
+
+        System.out.print("Enter Second Number:");
+        int number2 = imput.nextInt();
+
+        int counter = number;
+        counter = counter - number2;
+        
+
+        System.out.println("Values:");
+        System.out.println("ADDITION: " + number + number2);
+        System. out. println("SUBTRACTION = " + counter);
+        System.out.println("MULTIPICATION: " + number * number2);
+        System.out.println("DIVISION: " + number / number2);
+    }
+}
